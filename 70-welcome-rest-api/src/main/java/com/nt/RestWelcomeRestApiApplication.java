c8e1ab0d7ce4eb0e9fa52e94f5080b090039aa01
Sakharam Chitte <chittesakharam628@gmail.com>
2026-09-10 09:38:01 +0530

    feat: add welcome rest api

---
+++ 70-welcome-rest-api/src/main/java/com/nt/RestWelcomeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWelcomeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWelcomeRestApiApplication.class, args);
+	}
+
+}
